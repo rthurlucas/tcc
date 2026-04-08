2fd061b8634c9c94637a88e7bcd4e1bb77feb61f
Arthur <140291017+rthurlucas@users.noreply.github.com>
2026-04-08 19:01:00 -0300

    Update FeedbackRepository.java

--- back-end/src/main/java/com/tcc/indigena/backend/repository/FeedbackRepository.java
+++ back-end/src/main/java/com/tcc/indigena/backend/repository/FeedbackRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-    // Não precisa escrever nada aqui! O Spring cria os códigos de salvar sozinho.
 }
